914bda335e9e69b281d19136b7afaa6c99f88f19
Abdulrahman Abbas <abdouliabbas75@gmail.com>
2025-04-20 16:50:39 +0100

    add forms.py

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+from .forms import UserCreationForm, UserChangeForm
 
 class UserAdmin(BaseUserAdmin):
-    model = User
+    form = UserChangeForm
+    add_form = UserCreationForm
+
     list_display = ('username', 'email', 'full_name', 'contact_number', 'is_staff')
     search_fields = ('username', 'email', 'full_name', 'contact_number')
-    fieldsets = BaseUserAdmin.fieldsets + (
-        (None, {'fields': ('full_name', 'contact_number')}),
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('full_name', 'contact_number')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active', 'groups', 'user_permissions')}),
     )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'full_name', 'contact_number', 'password1', 'password2'),
+        }),
+    )
+
+    ordering = ('email',)
 
 admin.site.register(User, UserAdmin)
